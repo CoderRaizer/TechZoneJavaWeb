0969c41b4467253bb4c82da4dd967434403dea4f
Võ Duy Khánh <voduykhanhnc@gmail.com>
2019-11-25 14:07:21 +0700

    Init Commit Project

---
+++ src/main/java/com/techzone/springmvc/instance/MyConstants.java
@@ -0,0 +1,14 @@
+package com.techzone.springmvc.instance;
+
+public class MyConstants {
+	
+	// Replace with your email here:  
+    public static final String MY_EMAIL = "voduykhanhnc@gmail.com";
+ 
+    // Replace password!!
+    public static final String MY_PASSWORD = "Khanh0931061891";
+ 
+    // And receiver!
+//    public static final String CLIENT_EMAIL = "conghungyp2508@gmail.com";
+
+}
